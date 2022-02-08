daacff9cc77bcb09a41577d373263f443fdd43c8
Istvan <17463906+purple0CaT@users.noreply.github.com>
2022-02-08 14:07:04 +0000

    jwt for user route

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -1,6 +1,14 @@
 import {
-  Body, Controller, Delete, Get, Param, Post, Put
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { UserEntity } from '../entities/user.entity';
 import { UserService } from '../services/user.service';
 
@@ -8,26 +16,29 @@ import { UserService } from '../services/user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() userBody: UserEntity) {
     return this.userService.create(userBody);
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.userService.findOne(id);
   }
 
-
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(@Param('id') id: number, @Body() userBody: UserEntity) {
     return this.userService.update(id, userBody);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.userService.remove(id);
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserService } from './services/user.service';
 import { UserController } from './controller/user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { JwtStrategy } from 'src/auth/guards/jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, JwtStrategy],
   exports: [UserService],
 })
 export class UserModule {}
